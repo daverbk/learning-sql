3c0012b5f414cc55a5d09daa0a00025cad786ea9
David Rabko <david.rabko@icloud.com>
2023-10-23 11:56:23 +0300

    Complete create_db.sql

---
+++ chapter1/create_db.sql
@@ -0,0 +1,16 @@
+# Create a new user
+CREATE USER 'user'@'localhost' IDENTIFIED BY '*******';
+
+# Grant all privileges on the user
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost';
+
+# Create a new db
+CREATE DATABASE bank;
+
+# Select new db
+USE bank;
+
+# Use "SOURCE :filepath" to import data
+
+# Check current date/time
+SELECT now() FROM dual;
