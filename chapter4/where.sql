bd1e9589b26e03303c7dc1c9959b067353cef079
Dave Rabko <davidryabko13@gmail.com>
2023-11-16 22:57:39 +0100

    Add chapter4 progress

--- chapter4/where.sql
+++ chapter4/where.sql
@@ -21,3 +21,78 @@ WHERE title = 'Teller' AND start_date < '2003-01-01';
 SELECT * FROM employee
 WHERE end_date is NULL
 AND (title = 'Teller' OR start_date < '2003-01-01');
+
+# WHERE true AND NOT (true OR true) -> false
+# WHERE true AND NOT (true OR false) -> false
+# WHERE true AND NOT (false OR true) -> false
+# WHERE true AND NOT (false OR false) -> true
+# WHERE false AND NOT (true OR true) -> false
+# WHERE false AND NOT (true OR false) -> false
+# WHERE false AND NOT (false OR true) -> false
+# WHERE false AND NOT (false OR false) -> false
+
+SELECT * FROM employee
+WHERE end_date is NULL
+AND NOT (title = 'Teller' OR start_date < '2003-01-01');
+
+SELECT * FROM employee
+WHERE end_date is NULL
+AND title != 'Teller' AND start_date >= '2003-01-01';
+
+# Equality condition
+
+SELECT pt.name product_type, p.name product
+FROM product p INNER JOIN product_type pt
+ON p.product_type_cd = pt.product_type_cd
+WHERE pt.name = 'Customer Accounts';
+
+# Inequality condition
+
+SELECT pt.name product_type, p.name product
+FROM product p INNER JOIN product_type pt
+ON p.product_type_cd = pt.product_type_cd
+WHERE pt.name != 'Customer Accounts';
+
+# Deletion using conditions
+
+DELETE FROM account
+WHERE status = 'CLOSED' AND YEAR(close_date) = 1999;
+
+# Range
+
+SELECT emp_id, fname, lname, start_date
+FROM employee
+WHERE start_date < '2003-01-01';
+
+SELECT emp_id, fname, lname, start_date
+FROM employee
+WHERE start_date < '2003-01-01'
+AND start_date >= '2001-01-01';
+
+# Between
+
+SELECT account_id, product_cd, cust_id, avail_balance
+FROM account
+WHERE avail_balance BETWEEN 3000 AND 5000;
+
+SELECT cust_id, fed_id
+FROM customer
+WHERE cust_type_cd = 'I'
+AND fed_id BETWEEN '500-00-0000' AND '999-99-9999';
+
+SELECT emp_id, fname, lname, start_date
+FROM employee
+WHERE start_date BETWEEN '2001-01-01' AND '2003-01-01';
+
+# Server the actually generates
+
+SELECT emp_id, fname, lname, start_date
+FROM employee
+WHERE start_date <= '2003-01-01'
+AND start_date >= '2001-01-01';
+
+# Set condition
+
+SELECT account_id, product_cd, cust_id, avail_balance
+FROM account
+WHERE product_cd IN('CHK', 'SAV', 'CD', 'MM');
