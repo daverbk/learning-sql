735028bfacc53f692d2349ab708050b4dafffd88
David Rabko <david.rabko@icloud.com>
2023-11-29 23:38:39 +0100

    Start chapter 5

--- chapter5/innerjoin.sql
+++ chapter5/innerjoin.sql
@@ -1,1 +1,8 @@
-SELECT DISTINCT * FROM department;
+# Cross join
+SELECT e.fname, e.lname, d.name
+FROM employee e JOIN department d;
+
+# Join ON
+SELECT e.fname, e.lname, d.name
+FROM employee e JOIN department d
+    ON e.dept_id = d.dept_id;
