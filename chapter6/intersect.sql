72ca6d9b2df8eec202991412fc9867fe2462350e
David Rabko <davidryabko13@gmail.com>
2023-12-11 09:10:52 +0100

    Add chapter6

---
+++ chapter6/intersect.sql
@@ -0,0 +1,20 @@
+# This query returns an empty set
+# as two sets are not intersecting in any way
+(SELECT emp_id, fname, lname
+FROM employee)
+INTERSECT
+SELECT cust_id, fname, lname
+FROM individual;
+
+# Would return emp_id 10
+SELECT emp_id
+FROM employee
+WHERE assigned_branch_id = 2
+    AND (title = 'Teller' OR title = 'Head Teller')
+INTERSECT
+SELECT DISTINCT open_emp_id
+FROM account
+WHERE open_branch_id = 2;
+
+# We can use INTERSECT ALL
+# that will preserve the duplicates if any exist
---
+++ chapter6/union.sql
@@ -0,0 +1,41 @@
+# A compound query with UNION set operator
+SELECT 1 num, 'abc' str
+UNION
+SELECT 9 num, 'xyz' str;
+
+# UNION ALL (all the records are included)
+SELECT cust_id, lname name
+FROM individual
+UNION ALL
+SELECT cust_id, name
+FROM business;
+
+# UNION ALL doesn't delete duplicates
+SELECT cust_id, lname name
+FROM individual
+UNION ALL
+SELECT cust_id, name
+FROM business
+UNION ALL
+SELECT cust_id, name
+FROM business;
+
+# Another example of duplicates preservation
+SELECT emp_id
+FROM employee
+WHERE assigned_branch_id = 2
+    AND (title = 'Teller' OR title = 'Head Teller')
+UNION ALL
+SELECT DISTINCT open_emp_id
+FROM account
+WHERE open_branch_id = 2;
+
+# UNION deletes the duplicates
+SELECT emp_id
+FROM employee
+WHERE assigned_branch_id = 2
+    AND (title = 'Teller' OR title = 'Head Teller')
+UNION
+SELECT DISTINCT open_emp_id
+FROM account
+WHERE open_branch_id = 2;
