754f35868b8397fab2fdfba2bd4f49fb448b3cc1
David Rabko <david.rabko@icloud.com>
2023-12-31 17:15:40 +0300

    Add 197

--- sql50/sql50.sql
+++ sql50/sql50.sql
@@ -36,3 +36,9 @@ FROM Visits
 WHERE visit_id NOT IN (SELECT visit_id FROM Transactions)
 GROUP BY customer_id;
 
+# 197. Rising Temperature
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
