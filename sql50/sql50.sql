7495d577894ce6381bffb07c9b45971ecc996480
David Rabko <david.rabko@icloud.com>
2024-01-14 21:21:57 +0100

    Add 1251. Average Selling Price

--- sql50/sql50.sql
+++ sql50/sql50.sql
@@ -85,4 +85,13 @@ SELECT *
 FROM Cinema
 WHERE MOD(id, 2) != 0
     AND description NOT LIKE '%boring%'
-ORDER BY rating DESC
+ORDER BY rating DESC;
+
+# 1251. Average Selling Price
+SELECT p.product_id, IFNULL(ROUND(SUM(u.units * p.price) / SUM(u.units), 2), 0) average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+    ON p.product_id = u.product_id
+    AND u.purchase_date BETWEEN start_date
+    AND end_date
+GROUP BY p.product_id;
