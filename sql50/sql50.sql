5329fa39e2dccd7689cb4fe688655122d130d3e7
David Rabko <davidryabko13@gmail.com>
2024-01-19 09:43:51 +0100

    Add 550. Game Play Analysis IV

--- sql50/sql50.sql
+++ sql50/sql50.sql
@@ -138,3 +138,9 @@ WHERE (customer_id, order_date) IN (
     FROM Delivery
     GROUP BY customer_id
 );
+
+# 550. Game Play Analysis IV
+SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) fraction
+FROM Activity
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+IN (SELECT player_id, MIN(event_date) AS first_login FROM ACTIVITY GROUP BY player_id);
