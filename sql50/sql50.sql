1d6eca7d9020ea5f600016456419b71d33710406
David Rabko <david.rabko@icloud.com>
2024-01-26 07:57:28 +0100

    Add 180. Consecutive Numbers

--- sql50/sql50.sql
+++ sql50/sql50.sql
@@ -228,3 +228,13 @@ CASE
   ELSE 'Yes'
 END triangle
 FROM triangle;
+
+# 180. Consecutive Numbers
+SELECT DISTINCT l1.Num ConsecutiveNums
+FROM Logs l1,
+    Logs l2,
+    Logs l3
+WHERE l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num;
