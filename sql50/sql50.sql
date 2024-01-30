d56f7456fdd5b1a518c9134e6935e779cdeb4cdf
David Rabko <david.rabko@icloud.com>
2024-01-30 09:21:23 +0100

    Add 1907. Count Salary Categories

--- sql50/sql50.sql
+++ sql50/sql50.sql
@@ -271,3 +271,13 @@ FROM Accounts
 UNION
 SELECT "High Salary" category, sum(income > 50000) accounts_count
 FROM Accounts;
+
+# 1978. Employees Whose Manager Left the Company
+SELECT e1.employee_id
+FROM Employees e1
+LEFT JOIN Employees e2
+    ON e1.manager_id = e2.employee_id
+WHERE e2.employee_id IS NULL
+    AND e1.manager_id IS NOT NULL
+    AND e1.salary < 30000
+ORDER BY employee_id;
