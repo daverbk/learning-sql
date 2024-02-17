7f62b65f87201b65d0367ea75dc25d93ad32a81f
David Rabko <david.rabko@icloud.com>
2024-02-17 09:15:39 +0100

    Add 585. Investments in 2016

--- sql50/sql50.sql
+++ sql50/sql50.sql
@@ -385,3 +385,18 @@ HAVING unit >= 100;
 SELECT user_id, name, mail
 FROM Users
 WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode[.]com$';
+
+# 585. Investments in 2016
+SELECT ROUND(SUM(tiv_2016), 2) tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(tiv_2015) > 1
+) AND (lat, lon) IN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+);
